b353c1ca5b7987244743e277ddea3e64034b0f8f
Simona Golebiewski <colfescus@yahoo.com>
2017-08-22 11:45:26 -0400

    earlybird markup and styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
 console.log('This is the Main.js file. It should be the 3rd and final file');
+
+$(function() {
+    var div = $(".staticHeader");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 140) {
+            div.removeClass('staticHeader').addClass("fixedHeader");
+        } else {
+            div.removeClass("fixedHeader").addClass('staticHeader');
+        }
+    });
+});
